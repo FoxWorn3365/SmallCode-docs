5dd4e5f37d6c428eb5da352e605ddc6cabc9273b
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:48:30 +0100

    Create file.copy.php

---
+++ methods/file.copy.php
@@ -0,0 +1,21 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-file' class='doc'>
+    <h1>Method: file.copy</h1>
+    Copy a file in another file.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     file.copy(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>file</lb>,
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>destination</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method file.copy('sus/amogus.txt', 'sus/uwu.txt')
+     </code>
+    </pre>
+   </div>
