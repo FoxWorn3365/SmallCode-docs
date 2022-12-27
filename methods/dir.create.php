30e7718fbf59ba26e1c5c7e1314f8a60d84cc45f
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:53:51 +0100

    Create dir.create.php

---
+++ methods/dir.create.php
@@ -0,0 +1,20 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-dir' class='doc'>
+    <h1>Method: dir.create</h1>
+    Make a new directory with default permission for web user<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     dir.create(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>dirName</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method dir.create('sus')
+     </code>
+    </pre>
+   </div>
