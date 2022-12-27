69957c0300faada5567d3697a04dad72f80e6a64
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:31:29 +0100

    Create native.author.php

---
+++ methods/native.author.php
@@ -0,0 +1,18 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-native' class='doc'>
+    <h1>Method: native.author</h1>
+    Get the author(s) of this SmallCode Version<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     native.author(
+     ) <gg>return: <a href='/docs/language/string'>string</a></gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     print method native.author()   // Output: FoxWorn3365
+     </code>
+    </pre>
