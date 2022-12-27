a0bc82b5c8b17ada6afd0d5681047b2777b33630
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:52:40 +0100

    Create dir.php

---
+++ methods/dir.php
@@ -0,0 +1,7 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-dir' class='doc'>
+    <h1>Method domain: dir || directory</h1>
+    <a href='/docs/methods/dir.create'><code>dir.create</code></a><br>
+    <a href='/docs/methods/dir.remove'><code>dir.remove/code></a><br>
+    <a href='/docs/methods/dir.get'><code>dir.scan</code></a><br>
+   </div>
