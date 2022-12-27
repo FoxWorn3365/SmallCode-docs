2ff9dea2362587be919704615d984da50431bd14
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:51:07 +0100

    Create file.move.php

---
+++ methods/file.move.php
@@ -0,0 +1,21 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-file' class='doc'>
+    <h1>Method: file.move</h1>
+    Move a file<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     file.write(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>from</lb>,
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>to</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method file.move('sus/amogus.txt', 'amogus.txt')
+     </code>
+    </pre>
+   </div>
