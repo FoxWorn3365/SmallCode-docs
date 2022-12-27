a3729bec043fa3f666fa263b4f37b6a02115af87
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:55:10 +0100

    Create dir.remove.php

---
+++ methods/dir.remove.php
@@ -0,0 +1,20 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-dir' class='doc'>
+    <h1>Method: dir.remove</h1>
+    Remove an empty directory. If the directory is not empty you can get file list with <a href='/docs/methods/dir.get'>dir.get</a>.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     dir.remove(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>dirName</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method dir.remove('sus')
+     </code>
+    </pre>
+   </div>
