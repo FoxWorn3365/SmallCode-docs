44e2275a4f70b46e1e01a642d05c4835dc2ffeb7
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:32:11 +0100

    Create native.website.php

---
+++ methods/native.website.php
@@ -0,0 +1,18 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-native' class='doc'>
+    <h1>Method: native.website</h1>
+    Get the used SmallCode version's website.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     native.website(
+     ) <gg>return: <a href='/docs/language/string'>string</a></gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     print method native.website()   // Output: https://smallcode.cf
+     </code>
+    </pre>
