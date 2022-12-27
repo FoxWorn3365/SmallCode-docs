77278e6b3b9aac4c0a2b0db24b6cc77b5d47637f
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:34:10 +0100

    Create linux.php

---
+++ methods/linux.php
@@ -0,0 +1,5 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-linux' class='doc'>
+    <h1>Method domain: linux</h1>
+    <a href='/docs/methods/linux.exec'><code>linux.exec</code></a><br>>
+   </div>
