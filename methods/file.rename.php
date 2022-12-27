3edf28272fe59909c53bcd119ab5f83f9d28ed1d
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:50:15 +0100

    Create file.rename.php

---
+++ methods/file.rename.php
@@ -0,0 +1,21 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-file' class='doc'>
+    <h1>Method: file.rename</h1>
+    Rename a file.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     file.write(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>oldName</lb>,
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>newName</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method file.write('sus/amogus.txt', 'sus/ewe.md')    // sus/amogus.txt will be renamed in sus/ewe.md
+     </code>
+    </pre>
+   </div>
