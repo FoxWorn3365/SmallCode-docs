b703a88239429925fad3b5cd139fcbcedc316c81
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:47:16 +0100

    Create file.delete.php

---
+++ methods/file.delete.php
@@ -0,0 +1,20 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-file' class='doc'>
+    <h1>Method: file.delete</h1>
+    Delete a file.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     file.delete(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>fileDirectory</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method file.delete('sus/amogus.txt')
+     </code>
+    </pre>
+   </div>
