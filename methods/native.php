6998361ad1dc3932ad909e6dc992b6e5deadc52c
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:29:21 +0100

    Create native.php

---
+++ methods/native.php
@@ -0,0 +1,8 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-native' class='doc'>
+    <h1>Method domain: native</h1>
+    <a href='/docs/methods/native.version'><code>native.version</code></a><br>
+    <a href='/docs/methods/native.author'><code>native.author</code></a><br>
+    <a href='/docs/methods/native.website'><code>native.website</code></a><br>
+    <a href='/docs/methods/native.busetto'><code>native.busetto</code></a><br>
+   </div>
