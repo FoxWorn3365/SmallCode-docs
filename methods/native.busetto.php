1a1c277a502c74b930f86f10af33595bfefe895d
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:33:21 +0100

    Create native.busetto.php

---
+++ methods/native.busetto.php
@@ -0,0 +1,18 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-native' class='doc'>
+    <h1>Method: native.busetto</h1>
+    An easter-egg.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     native.busetto(
+     ) <gg>return: <a href='/docs/language/string'>string</a></gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     print method native.busetto()
+     </code>
+    </pre>
