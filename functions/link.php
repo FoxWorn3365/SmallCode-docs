e8bcf51aaf09f784055d0641f681c714e1f86398
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 22:33:21 +0100

    Create link.php

---
+++ functions/link.php
@@ -0,0 +1,13 @@
+<?php require 'assets/header.php' ?>
+   <div id='link' class='doc'>
+    <h1>link</h1>
+    Create a pointer.<br><br>
+    <h2>Example:</h2>
+    <pre>
+     <code>
+    link [VAR1] with [VAR2]
+     </code>
+    </pre>
+    <br><br>
+    <h4>Code examples</h4>
+    For examples please visit <a href='/docs/language/pointer'>this page</a>.
