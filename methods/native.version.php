90e90ee24329184af21fda155a15e3740e0bf522
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-27 19:30:36 +0100

    Create native.version.php

---
+++ methods/native.version.php
@@ -0,0 +1,18 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-native' class='doc'>
+    <h1>Method: native.version</h1>
+    Get the used SmallCode version.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     native.version(
+     ) <gg>return: <a href='/docs/language/string'>string</a></gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     print method native.version()   // Output: 0.9
+     </code>
+    </pre>
