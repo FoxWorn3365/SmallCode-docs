fafbad7dcbae6357f43674be31eefdffc2d7c9f2
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-28 23:24:37 +0100

    Create config.php

---
+++ language/config.php
@@ -0,0 +1,17 @@
+<?php require 'assets/header.php'; ?>
+   <div id='lan-config' class='doc'>
+    <h1>Language: SmallCode Coonfiguration</h1>
+    SmallCode, mainly for events, imports a special configuration that can easily be modified directly from the language file or locally using the <a href='/docs/functions/config'>config function</a>.
+    <pre><code>
+    public array config
+     |
+     +--events (array)
+          |
+          +--timeout (int)
+          |
+          +--checkspeed (int)
+    </code></pre>
+    <br><br>
+  </div>
+</body>
+</html>
