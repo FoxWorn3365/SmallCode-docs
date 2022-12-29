26d20cf93bf1560f36a02e4d8580444427e1d571
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-30 00:04:43 +0100

    Create headers.php

---
+++ methods/headers.php
@@ -0,0 +1,6 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-head' class='doc'>
+    <h1>Method domain: headers</h1>
+    <a href='/docs/methods/headers.get'><code>headers.get</code></a><br>
+    <a href='/docs/methods/headers.set'><code>headers.set/code></a>
+   </div>
