c29b530c49979a9d90835a3bb0490f0c9bf3a70d
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-29 23:54:40 +0100

    Create string.join.php

---
+++ methods/string.join.php
@@ -0,0 +1,24 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-string' class='doc'>
+    <h1>Method: string.join</h1>
+    Join an array into a string with a "join" character(s).<br>
+    <b>&#9888;&#65039; This method requires the implementation of simple arrays (lists) and therefore at the moment it is not yet available, even if a simple array is provided by its opposite <a href='/docs/method/string.split'>string.split</a> and in that case it is OK to use it</b>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     string.join(
+        <a href='/docs/dis/string_var' class='g'>string|var</a> <lb>joinChar</lb>,
+        <a href='/docs/language/array' class='g'>array</a> <lb>array</lb>
+     ) <gg>return: <a href='/docs/language/string'>string</a></gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     //
+  
+     //
+     </code>
+    </pre>
+   </div>
