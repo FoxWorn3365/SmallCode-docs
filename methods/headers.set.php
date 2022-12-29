f4b965ddb4d2706bb09dcd94de802197a5c93413
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-30 00:07:24 +0100

    Create headers.set.php

---
+++ methods/headers.set.php
@@ -0,0 +1,21 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-head' class='doc'>
+    <h1>Method: headers.set</h1>
+    Set headers of the response.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     headers.set(
+        <a href='/docs/dis/string_var' class='g'>string|var</a> <lb>name</lb>
+        <a href='/docs/dis/string_var' class='g'>string|var</a> <lb>value</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method headers.set('X-Powered-By', 'SmallCode')
+     </code>
+    </pre>
+   </div>
