d6af07210f50390df174c490be580c9f7e09ebf9
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-29 23:21:05 +0100

    Create string.php

---
+++ methods/string.php
@@ -0,0 +1,9 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-string' class='doc'>
+    <h1>Method domain: string</h1>
+    <a href='/docs/methods/string.replace'><code>string.replace</code></a><br>
+    <a href='/docs/methods/string.split'><code>string.split</code></a><br>
+    <a href='/docs/methods/string.join'><code>string.replace</code></a><br>
+   </div>
+ </body>
+</html>
