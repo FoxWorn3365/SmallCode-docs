06eeca167cffbea4b677f354d579e66f8464c3ad
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 22:20:23 +0100

    Create main.php

---
+++ examples/main.php
@@ -0,0 +1,8 @@
+<?php require 'assets/header.php'; ?>
+   <div id='examples' class='doc highlight'>
+    <h1>SmallCode Examples Root</h1>
+    This page will list all the publicly available examples also taken from projects under development. Special thanks to <b><a href='https://scpfoundation.it'>Foundation</a></b> for providing some examples
+    <h2>List</h2>
+    <div class='aligned-at-1'>
+     <a href='/docs/examples/authentication-with-an-external-api'>Authentication with SmallCode and external API</a><br>
+     <a href='/docs
