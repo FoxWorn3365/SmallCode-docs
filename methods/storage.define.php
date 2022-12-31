012db4380c010601f0c4bd0542da2e87842e8c08
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 20:37:00 +0100

    Create storage.define.php

---
+++ methods/storage.define.php
@@ -0,0 +1,20 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-storage' class='doc'>
+    <h1>Method: storage.define</h1>
+    Define and intialize a new storage.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     storage.define(
+        <a href='/docs/language/dis/string_var' class='g'>string|var</a> <lb>storageName</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     method storage.define('ciao')   // Now the storage called 'ciao' is defined
+     </code>
+    </pre>
+   </div>
