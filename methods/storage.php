4b2e41c436c866d562d164a55c5089c3d47225d6
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 20:35:32 +0100

    Create storage.php

---
+++ methods/storage.php
@@ -0,0 +1,7 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-storage' class='doc'>
+    <h1>Method domain: storage</h1>
+    <a href='/docs/methods/storage.define'><code>storage.define</code></a><br>
+    <a href='/docs/methods/storage.get'><code>storage.get</code></a><br>
+    <a href='/docs/methods/storage.set'><code>storage.set</code></a><br>
+   </div>
