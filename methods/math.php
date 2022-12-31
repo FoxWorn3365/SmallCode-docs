42f517f2a8f3d9a0a7100d9d40515e49d552eddf
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 21:41:18 +0100

    Create math.php

---
+++ methods/math.php
@@ -0,0 +1,6 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-loop' class='doc'>
+    <h1>Method domain: math</h1>
+    <a href='/docs/methods/math.operation'><code>math.operation</code></a><br>
+    <a href='/docs/methods/math.expression'><code>math.expression</code></a>
+   </div>
