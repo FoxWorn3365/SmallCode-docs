4a97b2b9d3243720adceefa889403ff7f157890c
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 22:08:20 +0100

    Create type.get.php

---
+++ methods/type.get.php
@@ -0,0 +1,22 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-type' class='doc'>
+    <h1>Method: type.get</h1>
+    Get a var type.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     type.get(
+        <a href='/docs/language/var' class='g'>var</a> <lb>var</lb>
+     ) <gg>return: null</gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     define uwu string '50'
+     
+     print method type.get(uwu)   // Output: string
+     </code>
+    </pre>
+   </div>
