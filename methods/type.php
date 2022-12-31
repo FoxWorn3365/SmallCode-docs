f67ea29827ee0f0d993085adcfbf87e116d6c472
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 22:05:14 +0100

    Create type.php

---
+++ methods/type.php
@@ -0,0 +1,6 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-type' class='doc'>
+    <h1>Method domain: type</h1>
+    <a href='/docs/methods/type.set'><code>type.set</code></a><br>
+    <a href='/docs/methods/type.get'><code>type.get</code></a><br>
+   </div>
