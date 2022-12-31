57c8994f97c941a7ba827f0ef8c595c313c71637
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 22:04:02 +0100

    Create math.expression.php

---
+++ methods/math.expression.php
@@ -0,0 +1,20 @@
+<?php require 'assets/header.php'; ?>
+   <div id='mth-math' class='doc'>
+    <h1>Method: math.expression</h1>
+    Do a math expression.<br>
+    <h3>Description</h3>
+    <pre>
+     <code>
+     math.expression(
+        <a href='/docs/language/dis/string_var' class='g'>int|var</a> <lb>expression</lb>
+     ) <gg>return: <a href='/docs/language/string'>int</a></gg>
+     </code>
+    </pre>
+    <br><br>
+    <h3>Example</h3>
+    <pre>
+     <code>
+     print method math.expression('(2*2)*5') // Output: 20
+     </code>
+    </pre>
+   </div>
