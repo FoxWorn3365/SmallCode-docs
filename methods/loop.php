8954c5728ec8acd8970d1d5f211612a4eac1264c
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-31 20:09:49 +0100

    Update loop.php

--- methods/loop.php
+++ methods/loop.php
@@ -2,4 +2,6 @@
    <div id='mth-loop' class='doc'>
     <h1>Method domain: loop</h1>
     <a href='/docs/methods/loop.getValue'><code>loop.getValue</code></a><br>
+    <a href='/docs/methods/loop.localStorage.get'><code>loop.localStorage.get</code></a><br>
+    <a href='/docs/methods/loop.localStorage.set'><code>loop.localStorage.set</code></a><br>
    </div>
