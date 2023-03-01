d09c9a65bbfeaf6c148bf4b7636c323e34aa8911
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2023-03-01 23:15:17 +0100

    Create unlink.php

---
+++ functions/unlink.php
@@ -0,0 +1,13 @@
+<?php require 'assets/header.php' ?>
+   <div id='link' class='doc'>
+    <h1>unlink</h1>
+    Remove a pointer.<br><br>
+    <h2>Example:</h2>
+    <pre>
+     <code>
+    unlink [VAR1 (pointer)]
+     </code>
+    </pre>
+    <br><br>
+    <h4>Code examples</h4>
+    For examples please visit <a href='/docs/language/pointer'>this page</a>.
